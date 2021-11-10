12a8c8eac7309bbb0e3e6977b493b1b7a807f599
HiroUrata <1712026@i.softbank.jp>
2021-11-10 15:13:30 +0900

    AlamofireProcess getItemDetailDatas　追加

--- RakuIchibaRankAPILesson/Model/AlamofireProcess.swift
+++ RakuIchibaRankAPILesson/Model/AlamofireProcess.swift
@@ -17,6 +17,40 @@ extension AlamofireProcess{
     
     public func getItemDetailDatas(targetAge:String?,targetGender:String?,completion: @escaping ([ItemDetailDatas]?,Error?) -> Void){
      
+        guard let age = targetAge else { return }
+        guard let gender = targetGender else { return }
+        
+        let apiKey = ""
+        
+        AF.request(apiKey, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON {[self] response in
+            
+            switch response.result{
+                
+            case .success:
+                
+                let detailData = JSON(response.result as Any)["Items"]
+                
+                for dataCount in 0..<detailData.count{
+                    
+                    if let getRank = detailData[dataCount]["Item"]["rank"].string,
+                       let getMediumImageUrls = detailData[dataCount]["Item"]["mediumImageUrls"]["imageUrl"].string,
+                       let getItemName = detailData[dataCount]["Item"]["itemName"].string,
+                       let getItemPrice = detailData[dataCount]["Item"]["itemPrice"].string{
+                        
+                        privateItemDetailArray.append(ItemDetailDatas(rank: getRank,
+                                                                      mediumImageUrls: getMediumImageUrls,
+                                                                      itemName: getItemName,
+                                                                      itemPrice: getItemPrice))
+                    }
+                
+                }
+                completion(privateItemDetailArray, nil)
+                
+            case .failure(let error):
+                
+                completion(nil, error)
+            }
+        }
         
     }
 }
